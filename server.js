deb19294b74536587b713129bf3c050a4186c00d
betxcircle <150256421+betxcircle@users.noreply.github.com>
2025-05-01 11:54:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -311,9 +311,9 @@ const startTurnTimer = (roomId) => {
         const winnerPlayer = room.players.find(player => player.symbol === winnerSymbol);
        const loserPlayers = room.players.filter(player => player.symbol !== winnerSymbol);
       
-        if (winnerPlayer && loserPlayer) {
+        if (winnerPlayer && loserPlayers) {
           const winnerUserId = winnerPlayer.userId;
-          const loserUserId = loserPlayer.userId;
+          const loserUserId = loserPlayers.userId;
           const gameResult = `${winnerPlayer.name} (${winnerSymbol}) wins!`;
 
           // Access the totalBet from the room object
